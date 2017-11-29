51523865f1d7ba29d8e5abdd64655e00e109a2cb
cledant <cledant@student.42.fr>
2017-11-29 22:29:44 +0100

    Added Audio class

---
+++ includes/Audio.hpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Audio.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cledant <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/04 16:13:19 by cledant           #+#    #+#             */
+/*   Updated: 2017/09/21 17:47:48 by cledant          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AUDIO_HPP
+# define AUDIO_HPP
+
+# include "SFML/Audio/Sound.hpp"
+# include "SFML/Audio/SoundBuffer.hpp"
+# include "GeneralException.hpp"
+# include <string>
+# include <iostream>
+# include <map>
+
+class Audio
+{
+	public :
+
+		Audio(void);
+		virtual ~Audio(void);
+		Audio(const Audio &src) = delete;
+		Audio &operator=(Audio const &rhs) = delete;
+
+		void loadSound(std::string const &name, std::string const &file);
+		void playSound(std::string const &name);
+		void stopSound(std::string const &name);
+		void pauseSound(std::string const &name);
+		void setLoop(std::string const &name, bool value);
+		void setVolume(std::string const &name, float value);
+
+		class FileOpenException : public GeneralException
+		{
+			public :
+
+				explicit FileOpenException(void);
+				explicit FileOpenException(std::string const &path);
+				virtual ~FileOpenException(void) throw();
+		};
+
+	private :
+
+		std::map<std::string, sf::SoundBuffer> _buffer_list;
+		std::map<std::string, sf::Sound>       _sound_list;
+
+		bool _getSoundBuffer(std::string const &name, sf::SoundBuffer *buff);
+		bool _getSound(std::string const &name, sf::Sound *buff);
+};
+
+#endif
---
+++ srcs/Audio.cpp
@@ -0,0 +1,144 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Audio.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cledant <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/02 12:14:31 by cledant           #+#    #+#             */
+/*   Updated: 2017/09/24 13:31:34 by cledant          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Audio.hpp"
+
+Audio::Audio(void)
+{
+}
+
+Audio::~Audio(void)
+{
+	std::map<std::string, sf::Sound>::iterator it;
+
+	for (it = this->_sound_list.begin(); it != this->_sound_list.end(); ++it)
+		it->second.stop();
+}
+
+void Audio::loadSound(std::string const &name, std::string const &file)
+{
+	sf::SoundBuffer buff;
+	sf::Sound       sound;
+
+	if (!buff.loadFromFile(file))
+		throw Audio::FileOpenException(name);
+	sound.setBuffer(buff);
+	this->_buffer_list.insert(std::pair<std::string, sf::SoundBuffer>(name, buff));
+	this->_sound_list.insert(std::pair<std::string, sf::Sound>(name, sound));
+}
+
+void Audio::playSound(std::string const &name)
+{
+	sf::Sound *sound = nullptr;
+
+	if (!this->_getSound(name, sound))
+	{
+		std::cout << "Sound unknown : " + name << std::endl;
+		return;
+	}
+	sound->play();
+}
+
+void Audio::stopSound(std::string const &name)
+{
+	sf::Sound *sound = nullptr;
+
+	if (!this->_getSound(name, sound))
+	{
+		std::cout << "Sound unknown : " + name << std::endl;
+		return;
+	}
+	sound->stop();
+}
+
+void Audio::pauseSound(std::string const &name)
+{
+	sf::Sound *sound = nullptr;
+
+	if (!this->_getSound(name, sound))
+	{
+		std::cout << "Sound unknown : " + name << std::endl;
+		return;
+	}
+	sound->pause();
+}
+
+void Audio::setLoop(std::string const &name, bool value)
+{
+	sf::Sound *sound = nullptr;
+
+	if (!this->_getSound(name, sound))
+	{
+		std::cout << "Sound unknown : " + name << std::endl;
+		return;
+	}
+	sound->setLoop(value);
+}
+
+void Audio::setVolume(std::string const &name, float value)
+{
+	sf::Sound *sound = nullptr;
+
+	if (!this->_getSound(name, sound))
+	{
+		std::cout << "Sound unknown : " + name << std::endl;
+		return;
+	}
+	sound->setVolume(value);
+}
+
+bool Audio::_getSoundBuffer(std::string const &name, sf::SoundBuffer *buff)
+{
+	std::map<std::string, sf::SoundBuffer>::iterator it;
+
+	if ((it = this->_buffer_list.find(name)) == this->_buffer_list.end())
+	{
+		if (name.compare(it->first) == 0)
+		{
+			buff = nullptr;
+			return (false);
+		}
+	}
+	buff    = &(it->second);
+	return (true);
+}
+
+bool Audio::_getSound(std::string const &name, sf::Sound *buff)
+{
+	std::map<std::string, sf::Sound>::iterator it;
+
+	if ((it = this->_sound_list.find(name)) == this->_sound_list.end())
+	{
+		if (name.compare(it->first) == 0)
+		{
+			buff = nullptr;
+			return (false);
+		}
+	}
+	buff    = &(it->second);
+	return (true);
+}
+
+Audio::FileOpenException::FileOpenException(std::string const &path)
+{
+	this->_msg = "Texture : Failed to find to open file : ";
+	this->_msg += path.c_str();
+}
+
+Audio::FileOpenException::FileOpenException(void)
+{
+	this->_msg = "Texture : Failed to find to open file";
+}
+
+Audio::FileOpenException::~FileOpenException(void) throw()
+{
+}
